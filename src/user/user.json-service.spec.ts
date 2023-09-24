9b1b6d39c554bb997694af9a6b28d940595abb2b
Sara Bevilacqua <sarabev6@gmail.com>
2023-09-24 18:41:20 +0200

    feat - added unit tests for add function

--- src/user/user.json-service.spec.ts
+++ src/user/user.json-service.spec.ts
@@ -13,17 +13,62 @@ describe('UserJSONService', () => {
         jest.resetAllMocks();
     });
 
-    describe('add', () => {        
+    describe('add', () => {     
+        it('should read json file using fs module', () => {
+            const users: User[] = [];
+            const stringifiedUsers = JSON.stringify(users);
+            const dummyBuffer = Buffer.from(stringifiedUsers);
+            
+            fsMock.readFileSync.mockReturnValueOnce(dummyBuffer);
+
+            sut.add('testUsername');
+
+            expect(fsMock.readFileSync).toHaveBeenCalledTimes(1);
+        });
+
         it('should throw an error if the username already exists', () => {
-            const readJSONFileSpy = jest.spyOn(sut as any, 'readJSONFile').mockReturnValueOnce([{ id: 1001, username: 'polo' }]);
-            const existingUsername = 'polo';
+            const readJSONFileSpy = jest.spyOn(sut as any, 'readJSONFile').mockReturnValueOnce([{ id: 1001, username: 'testUsername' }]);
+            const existingUsername = 'testUsername';
 
             expect(() => {
               sut.add(existingUsername);
             }).toThrow('User already exists');
 
             expect(readJSONFileSpy).toHaveBeenCalled();
-          });
+        });
+
+        it('should add a user with a valid ID', () => {
+            const username = 'testUsername';
+            jest.spyOn(Math, 'random').mockReturnValue(0.6);
+            
+            const newUser = sut.add(username);
+            
+            const expectedUser = {
+                id: 6000,
+                username: username,
+            };
+            
+            expect(newUser.id).toEqual(expectedUser.id);
+            expect(newUser.username).toEqual(expectedUser.username);
+        });
+        
+        it('should save the added user to the JSON file', () => {
+            const username = 'testUsername';
+            jest.spyOn(Math, 'random').mockReturnValue(0.6);
+            const writeFileSyncMock = jest.spyOn(fs, 'writeFileSync');
+            
+            sut.add(username);
+            
+            const expectedUser = {
+                id: 6000,
+                username: username,
+            };
+            
+            expect(writeFileSyncMock).toHaveBeenCalledWith(
+                sut['filePath'],
+                JSON.stringify([expectedUser])
+            );
+        });
     });
 
     describe('getById', () => {
@@ -35,6 +80,7 @@ describe('UserJSONService', () => {
             fsMock.readFileSync.mockReturnValueOnce(dummyBuffer);
 
             sut.getById(0);
+
             expect(fsMock.readFileSync).toHaveBeenCalledTimes(1);
         });
     });
