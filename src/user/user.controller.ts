c232c89b0028c7fdb5a0d29291f703e9334e2e89
Sara Bevilacqua <sarabev6@gmail.com>
2023-09-30 19:19:39 +0200

    feat - added checks in user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,13 +8,19 @@ export class UserController {
         if (!username){
             throw new Error('Username is empty');
         }
+        if (username.trim() === '') {
+            throw new Error('Username should contain characters other than whitespace');
+        }
         return this.userService.add(username);
     }
 
     getById(id: number): User | null {
-        if(!Number.isInteger(id)) {
+        if(Number.isInteger(id)) {
             throw new Error('Id is not an integer');
-        } 
+        }
+        if (id < 0) {
+            throw new Error('Id should be positive');
+        }
         return this.userService.getById(id);
     }
 }
